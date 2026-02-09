4693ebb194d770fb55b055c6b1a92c5795f5f6d0
edwin10045 <202400236@upqroo.edu.mx>
2026-02-09 16:07:19 -0500

    Clean up PHPMailerAutoload.php comments
    Removed legacy compatibility comments from the autoload file.

--- PHPMailerAutoload.php
+++ PHPMailerAutoload.php
@@ -1,7 +1,6 @@
 <?php
-// PHPMailer loader for legacy compatibility
-// Use Composer for modern projects: composer require phpmailer/phpmailer
 require 'PHPMailer/PHPMailer.php';
 require 'PHPMailer/SMTP.php';
 require 'PHPMailer/Exception.php';
 ?>
+
